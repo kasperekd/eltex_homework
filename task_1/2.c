6eb73e23e18b1215a8b2a89e62a70eab03a10a9f
kaseprekd <kasperekd@mail.ru>
2024-06-23 23:26:12 +0700

    2

---
+++ task_1/2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("\nEnter a negative integer\n> ");
+
+    int in = 0;
+    int try_count = 0;
+    int c = 0;
+
+    while (1){
+        if ((scanf("%d", &in) == 1) && (in < 0)){
+            break;
+        }
+        
+        try_count++;
+
+        if (try_count < 3){
+            printf("\nInvalid number entered, try again\n> ");
+            while ((c = getchar()) != '\n' && c != EOF);
+            continue;
+        }else{
+            printf("\nToo many failed input attempts\n");
+            exit(1);
+        }
+    }
+    
+    int int_size = sizeof(in) * 8;
+    
+    char buf[int_size + 1];
+    buf[int_size] = '\0';
+
+    for (int i = 0; i < int_size; i++){
+        buf[int_size - 1 - i] = ((in >> i) & 1) + '0';
+    }
+    
+    printf("\nBinary representation of a negative integer: %s\n", buf);
+
+    return 0;
+}
